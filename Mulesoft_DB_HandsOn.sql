872a7d44818e86c7f0ec732f3d07a39faf410636
technicallearning <learning.learning23@gmail.com>
2025-08-28 23:32:02 +0530

    Hands On Checkin

---
+++ Mulesoft_DB_HandsOn.sql
@@ -0,0 +1,17 @@
+/*USE mulesoft_db;
+GO
+
+CREATE SCHEMA mulesoft_course;
+GO
+
+CREATE TABLE mulesoft_course.customer (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    customerName VARCHAR(255) NOT NULL
+);
+GO*/
+
+select * from mulesoft_course.customer;
+
+-- Insert 5 random rows into mulesoft_course.customer
+
+
